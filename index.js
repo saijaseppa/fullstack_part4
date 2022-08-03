9cdf6df12700017383990aa87b6237bc135c370e
Saija <saija.seppa@tuni.fi>
2022-08-03 13:09:09 +0300

    4.2

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-
-const logger = require('./utils/logger')
 const config = require('./utils/config')
-//const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-/*app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})*/
-
-/*app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})*/
+const server = http.createServer(app)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`)
 })
+
--- models/blog.js
+++ models/blog.js
@@ -2,18 +2,6 @@ const mongoose = require('mongoose')
 const config = require('../utils/config')
 const logger = require('../utils/logger')
 
-const mongoUrl = config.MONGODB_URI
-
-logger.info('connecting to mongo')
-mongoose.connect(mongoUrl)
-    .then(result => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
-
-
 const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
